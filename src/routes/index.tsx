c8f7d7fe5571431f6e7e7673c0b4ed1a535d9cb0
André Coelho <andre.ribeiro@iv2.com.br>
2020-02-06 16:43:01 -0300

    refactor: redirect route not fount

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 // pages
 import Artist from '../pages/Artist';
@@ -8,6 +8,7 @@ const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Artist} />
+      <Redirect to="/" />
     </Switch>
   </BrowserRouter>
 );
