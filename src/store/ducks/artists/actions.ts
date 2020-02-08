687b6314ddc7e78d4033d45ab0ddb01a932b16fa
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-02-08 11:39:24 -0300

    remove comments

--- src/store/ducks/artists/actions.ts
+++ src/store/ducks/artists/actions.ts
@@ -1,7 +1,6 @@
 import { action } from 'typesafe-actions';
 import { ArtistsTypes, IArtist } from './types';
 
-// primeiro parametro é o type do action e o segundo um pay load se tiver
 export const loadRequest = (search: string) =>
   action(ArtistsTypes.LOAD_REQUEST, { search });
 export const loadSuccess = (data: IArtist) =>
