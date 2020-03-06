84a53fd2f717b728423b901b3b40349df7df3a75
mouragguilab <mouragguilab@gmail.com>
2020-03-06 11:16:49 -0300

    serviceWorker configuration

---
+++ public/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_VERSION = 6;
+const CACHE_NAMES = {
+  default: `spotifynder-cache-v${CACHE_VERSION}`,
+};
+
+self.addEventListener('install', function(event) {
+  event.waitUntil((async () => {
+    try {
+      const cache = await caches.open(CACHE_NAMES.default);
+      const assets = await fetch('asset-manifest.json').then(res => res.json());
+      
+      const files = [
+        ...Object.values(assets.files),
+        ...Object.values(assets.entrypoints),
+      ];
+
+      event.waitUntil((async () => {
+        return files
+          .filter(async (file) => await cache.match(file))
+          .map(async (file) => cache.add(file));
+      })());
+    } catch (err) {
+      console.log('Error while searching for assets!', err);
+    }
+  })());
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil((async () => {
+    const NAMES = Object.values(CACHE_NAMES);
+    const cacheNames = await caches.keys();
+    const deletePromises = cacheNames
+      .filter(cacheName => NAMES.includes(cacheName))
+      .map((cacheName) => caches.delete(cacheName))
+
+    await Promise.all(deletePromises)
+  })());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  };
+
+  if (!/https?/.test(event.request.url)) {
+    return;
+  }
+
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAMES.default);
+    const cacheResponse = await cache.match(event.request);
+
+    if (cacheResponse) {
+      return cacheResponse;
+    }
+
+    cache.add(event.request)
+      .catch(() => {});
+    
+    return fetch(event.request);
+  })());
+});
