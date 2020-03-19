bf7177479c890300815fc21984929e085bc61f21
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2020-03-19 16:11:09 -0300

    :clown_face: mock added

---
+++ __mocks__/artist.js
@@ -0,0 +1,19 @@
+export const albums = [
+  {
+    id: 1,
+    name: 'Live 2012 - O2 Shepherds Bush Empire',
+    image: '#',
+  },
+  {
+    id: 2,
+    name: 'Live at High Voltage Festival 2011',
+    image: '#',
+  },
+];
+
+export const artist = {
+  id: 1,
+  name: 'Thin Lizzy',
+  image: '#',
+  albums,
+};
