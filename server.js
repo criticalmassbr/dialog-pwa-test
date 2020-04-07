0b5de8be235c6a005770b9d82d7b338bb31590b0
Felipe Trova <felipee.trova@gmail.com>
2020-04-07 19:18:56 -0300

    Change server to out build

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/out/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'out')));
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'out', 'index.html'));
 });
 app.listen(port);
