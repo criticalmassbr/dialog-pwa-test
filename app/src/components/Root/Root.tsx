36c67274df2f61bd24dc16c5e117cfee79d15fb2
ZarathosDeath <nando_rhapsody@hotmail.com>
2020-09-03 13:55:42 -0300

    add Root component

---
+++ app/src/components/Root/Root.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const Heading = styled.strong`
+  display: block;
+  font-size: 2rem;
+  margin: 0.75em 0 1em;
+`
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 60rem;
+`
+
+const Root = () => {
+  return <Wrapper>
+    <Heading>MySocial</Heading>
+  </Wrapper>
+}
+
+export default Root
