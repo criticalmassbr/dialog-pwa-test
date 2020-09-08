9320419f8faeff5f8d8bc6117646d4a6dd55ea80
Ricardo Ferreira <ricardoff5@hotmail.com>
2020-09-08 09:34:42 -0300

    Migrating to CSS GRID using styled-component

--- react-pwa-front-v1/src/components/Container/Container.jsx
+++ react-pwa-front-v1/src/components/Container/Container.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import styled from 'styled-components'
 
-import './Container.css'
+const Container = styled.div`
+    width: 100%;
+    max-width: 1280px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(4, minmax(100px,1fr));
+    grid-auto-rows: auto;
+    grid-gap: 1rem;
+
+    @media(max-width: 800px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`
 
 export default (props) => {
     const { children } = props
     return (
-        <div className='container'>
+        <Container>
             {children}
-        </div>
+        </Container>
     );
 }
