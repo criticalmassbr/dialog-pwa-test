40e77747e73640ac19e51d2abcffaba77e22c470
Cosme Lorim <cosme.lorim@gmail.com>
2021-04-29 05:28:54 -0300

    Adiciona eslint e prettier

---
+++ front-web/src/prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // Força utilização de aspas simples
+  trailingComma: 'all', // Força utilização da virgula no fim de objetos
+  arrowParens: 'avoid', // Força não utilização de parênteses com funções de um único parâmetro.
+}
