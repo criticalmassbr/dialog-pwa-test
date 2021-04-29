1b319607b542bf7ae5a93b5fae9de9b574943f2a
Cosme Lorim <cosme.lorim@gmail.com>
2021-04-29 05:45:00 -0300

    Adiciona reat-router

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import GlobaStyle from './global';
 import './App.css';
 
+const Root = () => <p>Welcome</p>
+const About = () => <p>About</p>
+const Home = () => <p>Home</p>
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <GlobaStyle />
@@ -20,6 +32,27 @@ function App() {
         >
           Learn React
         </a>
+        <p>Contador: {count}</p>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <Router>
+          <nav>
+            <ul>
+              <li><Link to="/home">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" exact>
+              <Root />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </header>
     </div>
   );
