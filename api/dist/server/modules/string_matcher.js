daf532875adf0457f88566617222ef7caeae9e3d
André <andregp96@hotmail.com>
2022-02-07 08:01:13 -0300

    added a empty space filter to string match

--- api/dist/server/modules/string_matcher.js
+++ api/dist/server/modules/string_matcher.js
@@ -2,8 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.matchName = void 0;
 function matchName(search_term, name) {
-    let splits = search_term.split(" ");
+    let splits = search_term.split(" ").filter(split => split != "");
     let patterns = splits.map(part => new RegExp(part, "i"));
+    console.log(splits, patterns, name);
     return patterns.some(patt => patt.test(name));
 }
 exports.matchName = matchName;
--- api/src/modules/string_matcher.ts
+++ api/src/modules/string_matcher.ts
@@ -1,6 +1,6 @@
 export function matchName(search_term: string, name: string){
-    let splits = search_term.split(" ");
-    let patterns = splits.map( part => new RegExp(part, "i"));
+    let splits = search_term.split(" ").filter( split => split != "");
+    let patterns = splits.map( part => new RegExp(part, "i")); console.log(splits, patterns, name)
 
     return patterns.some( patt => patt.test(name))
 }
