e6b08e49894fb54e4bced458f21a2fa3dd75b285
André <andregp96@hotmail.com>
2022-02-07 13:48:46 -0300

    Removed console logs

--- api/dist/server/modules/log.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logQuery = void 0;
-function logQuery(query) {
-    console.log(query);
-}
-exports.logQuery = logQuery;
--- api/dist/server/modules/string_matcher.js
+++ api/dist/server/modules/string_matcher.js
@@ -4,7 +4,6 @@ exports.matchName = void 0;
 function matchName(search_term, name) {
     let splits = search_term.split(" ").filter(split => split != "");
     let patterns = splits.map(part => new RegExp(part, "i"));
-    console.log(splits, patterns, name);
     return patterns.some(patt => patt.test(name));
 }
 exports.matchName = matchName;
