8922285c107e4b1f47d470cce37be452576a7d96
ElderSam <eldersamuel98@gmail.com>
2022-03-07 18:33:48 -0300

    refactor: src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,3 @@
-import { createServer } from "http";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-
 import { startServerAndListen } from "./server";
 
 startServerAndListen();
