31df0cd6d096754c8b6dc99e46490a0bf7a06b55
ElderSam <eldersamuel98@gmail.com>
2022-03-07 18:26:00 -0300

    feat: create ApolloServer & add the first graphQL query

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,51 @@
-console.log('server running!')
+import { createServer } from "http";
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+// 1
+const startServer = async () => { 
+
+  // 2
+  const app = express()
+  const httpServer = createServer(app)
+
+  // 3
+  const typeDefs = gql`
+    type Query {
+      hello: String
+      message: String
+      status: Int
+    }
+  `;
+
+  // 4
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello world',
+      message: () => 'server running!',
+      status: () => 200,
+    },
+  };
+
+  // 5
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  })
+
+  // 6
+  await apolloServer.start()
+
+  // 7
+  apolloServer.applyMiddleware({
+      app,
+      path: '/api' // or /graphql
+  })
+
+  // 8
+  httpServer.listen({ port: process.env.PORT || 4000 }, () =>
+    console.log(`Server listening on http://localhost:4000${apolloServer.graphqlPath}`)
+  )
+}
+
+startServer()
